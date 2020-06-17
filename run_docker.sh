355fdc97e6958287d6dd0a4ac556f4be1e9ab07c
abrahamfov <abrahamfov@gmail.com>
2020-06-18 00:42:55 +0200

    New features: run_docker.sh, docker_out.txt Fixes: app.py, requirements.txt

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build - --tag=MLmicroservice
+docker build . --tag=ml-microservice
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -it --rm --name MLmicroservice -p 8000:80 MLmicroservice
+docker run -it --rm --name ml-microservice -p 8000:80 ml-microservice
