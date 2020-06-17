5677629047db2653bb0c9332deeebf63365b7fce
abrahamfov <abrahamfov@gmail.com>
2020-06-18 01:05:04 +0200

    Fix: run_kuberntes.sh Fixed deployment name

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -17,7 +17,7 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward deployment/microservice 8000:80
+kubectl port-forward deployment/ml-microservice 8000:80
 
 # Get logs:
 kubectl logs 'kubectl get pods -o=name'
